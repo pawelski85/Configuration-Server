4533ec909710e970d15fb9fa784289937f418464
Pawel <pawel.tomczyk85@gmail.com>
2021-06-28 18:09:15 +0200

    init

---
+++ src/test/java/com/kodilla/cloud/CloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootTest
+class CloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
